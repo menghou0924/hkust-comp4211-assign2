79682d9f3282209999da9fd7bb83e3ad8a9de0fc
Raymond Sak <menghou0924@gmail.com>
2016-04-24 22:40:08 +0800

    Initial Commit

---
+++ hkust-comp-4321-assign2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hkust-comp-4321-assign2
+//
+//  Created by Raymond Sak on 24/4/2016.
+//  Copyright © 2016 Raymond Sak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
